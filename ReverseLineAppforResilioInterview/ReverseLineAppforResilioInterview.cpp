55c2a925a815613609ad2a0146d0d724a0f5d0e4
Lucas Dunks <ludunks@gmail.com>
2024-02-18 21:53:34 -0600

    Cleaned some leftover code for readability and finished out comments

--- ReverseLineAppforResilioInterview/ReverseLineAppforResilioInterview.cpp
+++ ReverseLineAppforResilioInterview/ReverseLineAppforResilioInterview.cpp
@@ -10,7 +10,7 @@ static void inputThreadFunction(std::ifstream* inputFile, std::queue<std::string
 	std::string inputLine;
 	if ((*inputFile).is_open())
 	{
-		while (std::getline(*inputFile, inputLine))
+		while (std::getline(*inputFile, inputLine))																		//iterate input of lines to queue until end of file
 		{
 			(*inputLines).push(inputLine);
 		}
@@ -22,7 +22,7 @@ static void inputThreadFunction(std::ifstream* inputFile, std::queue<std::string
 static void reverseThreadFunction(bool* inProgress, std::queue<std::string>* inputLines, std::queue<std::string>* outputLines)
 {
 	std::string inputLine;
-	while (*inProgress || !(*inputLines).empty())
+	while (*inProgress)																									//iterate reversal of lines in queue until previous thread is comfirmed to terminate
 	{
 		if (!(*inputLines).empty())
 		{
@@ -32,20 +32,32 @@ static void reverseThreadFunction(bool* inProgress, std::queue<std::string>* inp
 			(*inputLines).pop();
 		}
 	}
+
+	return;
 }
 
 static void outputThreadFunction(bool* inProgress, std::ofstream* outputFile, std::queue<std::string>* outputLines)
 {
-	while (*inProgress || !(*outputLines).empty())
+	bool firstLine = true;
+	while (firstLine)																									//used separate loop for first line to prevent preceeding or trailing line break in the output file
 	{
 		if (!(*outputLines).empty())
 		{
 			*outputFile << (*outputLines).front();
+			(*outputLines).pop();
+			firstLine = false;
+		}
+	}
+	while (*inProgress)																									//iterate output of lines from queue until previous thread is comfirmed to terminate
+	{
+		if (!(*outputLines).empty())
+		{
 			*outputFile << "\n";
+			*outputFile << (*outputLines).front();
 			(*outputLines).pop();
 		}
 	}
-
+	return;
 }
 
 int main()
@@ -57,19 +69,20 @@ int main()
 	outputFile.open("ReversedText.txt");																				//open output stream
 
 	std::queue<std::string> inputLines;																					//queues to share data between threads while maintaining proper order of text lines
-	std::queue<std::string> outputLines;
+	std::queue<std::string> outputLines;																				//
 
-	bool inputInProgress = true;																						//booleans to terminate thread loops as they complete
-	bool reverseInProgress = true;
+	bool inputInProgress = true;																						//booleans to terminate successive thread loops as they complete
+	bool reverseInProgress = true;																						//
 
 	std::thread inputThread(inputThreadFunction, &inputFile, &inputLines);												//first thread to handle input from file
 	std::thread reverseThread(reverseThreadFunction, &inputInProgress, &inputLines, &outputLines);						//second thread to handle reverse string operation
 	std::thread outputThread(outputThreadFunction, &reverseInProgress, &outputFile, &outputLines);						//third thread to hanle output to file
 
-	inputThread.join();
-	inputInProgress = false;
-	reverseThread.join();
-	reverseInProgress = false;
-	outputThread.join();
+	inputThread.join();																									//wait for input thread to complete
+	inputInProgress = false;																							//notify text reversal thread that input is complete
+	reverseThread.join();																								//wait for reverse thread to complete
+	reverseInProgress = false;																							//notify output threat that text reversal is complete
+	outputThread.join();																								//wait for output thread to complete
 
+	return 0;																											//terminate main
 }

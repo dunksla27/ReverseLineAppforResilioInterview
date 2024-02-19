a01de9eb6cec8648b164d80159fadb9b2e30dc7d
Lucas Dunks <ludunks@gmail.com>
2024-02-18 21:24:10 -0600

    Add project files.

---
+++ ReverseLineAppforResilioInterview/ReverseLineAppforResilioInterview.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <thread>
+
+
+static void inputThreadFunction(std::ifstream* inputFile, std::queue<std::string>* inputLines)
+{
+	std::string inputLine;
+	if ((*inputFile).is_open())
+	{
+		while (std::getline(*inputFile, inputLine))
+		{
+			(*inputLines).push(inputLine);
+		}
+	}
+
+}
+
+static void reverseThreadFunction(bool* inProgress, std::queue<std::string>* inputLines, std::queue<std::string>* outputLines)
+{
+	std::string inputLine;
+	while (*inProgress || !(*inputLines).empty())
+	{
+		if (!(*inputLines).empty())
+		{
+			inputLine = (*inputLines).front();
+			std::reverse(inputLine.begin(), inputLine.end());
+			(*outputLines).push(inputLine);
+			(*inputLines).pop();
+		}
+	}
+}
+
+static void outputThreadFunction(bool* inProgress, std::ofstream* outputFile, std::queue<std::string>* outputLines)
+{
+	while (*inProgress || !(*outputLines).empty())
+	{
+		if (!(*outputLines).empty())
+		{
+			*outputFile << (*outputLines).front();
+			*outputFile << "\n";
+			(*outputLines).pop();
+		}
+	}
+
+}
+
+int main()
+{
+	std::ifstream inputFile;																							//declare input file variable
+	inputFile.open("LinesForTesting.txt");																				//open input stream
+
+	std::ofstream outputFile;																							//declare output file variable
+	outputFile.open("ReversedText.txt");																				//open output stream
+
+	std::queue<std::string> inputLines;																					//queues to share data between threads while maintaining proper order of text lines
+	std::queue<std::string> outputLines;
+
+	bool inProgress = true;																								//booleans to terminate thread loops as they complete
+
+	std::thread inputThread(inputThreadFunction, &inputFile, &inputLines);												//first thread to handle input from file
+	std::thread reverseThread(reverseThreadFunction, &inProgress, &inputLines, &outputLines);							//second thread to handle reverse string operation
+	std::thread outputThread(outputThreadFunction, &inProgress, &outputFile, &outputLines);								//third thread to hanle output to file
+
+}
